0a6399f08e974d1e4cfafa46b7b0b2112fae6328
Gabriela Diaz <gdiazz@est.ups.edu.ec>
2024-09-20 12:12:10 -0500

    Commit Inicial

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Deber1_poo_FILE C:/QT-GABY/Deber1_poo/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Deber1_poo.exe)
+set(__QT_DEPLOY_TARGET_Deber1_poo_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Deber1_poo_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
